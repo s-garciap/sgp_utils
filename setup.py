e0749dd59bc6ccf79d778630bc567376b553d1ca
s-garciap <sgarciap@unizar.es>
2023-05-11 18:29:07 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
+from sgp_utils import __version__
 
 setup(
     name='sgp_utils',
-    version='0.1',
+    version=__version__,
     description='Paquete para trabajar más comodamente en python y postgis',
-    url='https://github.com/nombre_usuario/mi_paquete',
+    url='https://github.com/s-garciap/sgp_utils',
     author='Sergio García-Pérez',
     author_email='sgarciap@unizar.es',
-    packages=['sgp_utils'],
+    py_modules=['sgp_utils'],
     install_requires=[
       'os',
       'sqlite3',
@@ -16,15 +17,5 @@ setup(
       'sqlalchemy',
       'psycopg2',
       'time'
-    ],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9'
     ],
 )
